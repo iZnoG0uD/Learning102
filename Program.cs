375749b7d867b98d89ecc3063760627e0f96c62e
iZnoG0uD <46227437+iZnoG0uD@users.noreply.github.com>
2022-01-05 10:40:44 +0500

    Выполнил второе задание

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите первое число:");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+char znak;
+
+Console.WriteLine("Введите знак операции: +, -, *, /");
+znak = Convert.ToChar(Console.ReadLine());
+
+switch (znak)
+{
+    case '+':
+        Console.WriteLine("{0} + {1} = {2}", number1, number2, (number1 + number2));
+        break;
+    
+    case '-':
+        Console.WriteLine("{0} - {1} = {2}", number1, number2, (number1 - number2));
+        break;
+
+    case '*':
+        Console.WriteLine("{0} * {1} = {2}", number1, number2, (number1 * number2));
+        break;
+
+    case '/':
+        Console.WriteLine("{0} / {1} = {2}", number1, number2, (number1 / number2));
+        break;
+
+    default:
+        Console.WriteLine("Введите правильный знак");
+        break;
+}
+Console.WriteLine("Усё!");
